2835ed38a3e78e2c23fd0d5417bf928ad2c9630d
Muhammad Ali Mazhar butt <57632737+MUHAMMAD-ALI-MAZHAR-BUTT@users.noreply.github.com>
2021-01-29 20:10:28 +0500

    Add files via upload

---
+++ Shutdown pc.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+//using namespace std;
+int main()
+{
+
+	//for windows
+system("c:\\windows\\system32\\shutdown /s /t 0");
+
+//for linux user
+//system("shutdown -P 2");
+
+//for windows xp
+//system("c:\\windows\\system32\\shutdown -s");
+
+}
+
+
+
+
+
+
+
+
+//for windows command proccesor is cmd
+//for linux it's terminal
+
+
