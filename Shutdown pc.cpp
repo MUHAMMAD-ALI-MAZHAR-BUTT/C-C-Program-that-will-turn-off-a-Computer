06ecf2273037ea6271c4e284de3281eb55eb5d29
Muhammad Ali Mazhar butt <57632737+MUHAMMAD-ALI-MAZHAR-BUTT@users.noreply.github.com>
2021-01-29 20:20:26 +0500

    Update Shutdown pc.cpp

--- Shutdown pc.cpp
+++ Shutdown pc.cpp
@@ -1,3 +1,4 @@
+//****************************************Subscribe Efficient Learning world ***************************************************************
 #include<stdio.h>
 #include<stdlib.h>
 //using namespace std;
@@ -5,13 +6,19 @@ int main()
 {
 
 	//for windows
-system("c:\\windows\\system32\\shutdown /s /t 0");
-
-//for linux user
-//system("shutdown -P 2");
-
-//for windows xp
-//system("c:\\windows\\system32\\shutdown -s");
+	//1) turn off pc after 30 to 60 seconds
+	system("c:\\windows\\system32\\shutdown /s");
+	//2) turn off pc after 0 seconds immediately
+        system("c:\\windows\\system32\\shutdown /s /t 0");
+        //3) turn off pc after specific time ie 2 minutes (120seconds)
+	 system("c:\\windows\\system32\\shutdown /s /t 120");
+	//--------------------------------------------------------
+	//for linux user
+	//system("shutdown -P 2");
+         //p means poweroff and 2 is the number of minutes after you want to turn off pc
+	//--------------------------------------------------------
+	//for windows xp
+	//system("c:\\windows\\system32\\shutdown -s");
 
 }
 
@@ -19,10 +26,9 @@ system("c:\\windows\\system32\\shutdown /s /t 0");
 
 
 
-
-
-
 //for windows command proccesor is cmd
-//for linux it's terminal
+//for linux command proccesor is terminal
 
+
+//Subscribe my youtube channel : efficient learning world
 
